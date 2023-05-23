fcccadfe505dcbfb286fe513c69fa2b7c774896f
sophiamziou <sofiamziou@gmail.com>
2023-05-23 15:31:20 +0200

    booh

--- src/main/java/org/java/tester/Calculator.java
+++ src/main/java/org/java/tester/Calculator.java
@@ -1,5 +1,49 @@
 package org.java.tester;
 
 public class Calculator {
+	
+	public float num1;
+	public float num2;
+	
+	public Calculator(float num1, float num2) {
+		setNum1(num1);
+		setNum2(num2);
+	}
+	
+	
+    public float getNum1() {
+		return num1;
+	}
 
+
+	public void setNum1(float num1) {
+		this.num1 = num1;
+	}
+
+
+	public float getNum2() {
+		return num2;
+	}
+
+
+	public void setNum2(float num2) {
+		this.num2 = num2;
+	}
+
+
+	public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+    
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+    
+    public float divide(float num1, float num2) {
+        return num1 / num2;
+    }
+    
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
 }
--- src/test/java/org/java/tester/CalculatorTest.java
+++ src/test/java/org/java/tester/CalculatorTest.java
@@ -1,5 +1,110 @@
 package org.java.tester;
 
-public class CalculatorTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+
+public class CalculatorTest {
+	
+	private Calculator c;
+	
+	@BeforeEach
+	void init() {
+		float num1 = 5;
+		float num2 = 3;
+		c = new Calculator(num1, num2);
+	}
+	
+	@RepeatedTest(10)
+	public void getAddTest() throws Exception {
+		
+		assumeTrue(c.getNum2() != 0);
+		
+		final float attRes = c.getNum1() / c.getNum2();
+		
+		final float res = c.add(c.getNum1(), c.getNum2());
+		
+		assertEquals(attRes, res, "Somma tra float");
+	}
+	
+	@RepeatedTest(10)
+	public void getAddExcTest() {
+		
+		assumeTrue(c.getNum2() == 0);
+		
+		assertThrows(Exception.class,
+				() -> c.add(c.getNum1(), c.getNum2()),
+				"Deve sollevare eccezione");
+	}
+	
+	@RepeatedTest(10)
+	public void getSubTest() throws Exception {
+		
+		assumeTrue(c.getNum2() != 0);
+		
+		final float attRes = c.getNum1() / c.getNum2();
+		
+		final float res = c.subtract(c.getNum1(), c.getNum2());
+		
+		assertEquals(attRes, res, "Sottrazione tra float");
+	}
+	
+	@RepeatedTest(10)
+	public void getSubExcTest() {
+		
+		assumeTrue(c.getNum2() == 0);
+		
+		assertThrows(Exception.class,
+				() -> c.subtract(c.getNum1(), c.getNum2()),
+				"Deve sollevare eccezione");
+	}
+	
+	
+	@RepeatedTest(10)
+	public void getDivTest() throws Exception {
+		
+		assumeTrue(c.getNum2() != 0);
+		
+		final float attRes = c.getNum1() / c.getNum2();
+		
+		final float res = c.divide(c.getNum1(), c.getNum2());
+		
+		assertEquals(attRes, res, "Divisione tra float");
+	}
+	
+	@RepeatedTest(10)
+	public void getDivExcTest() {
+		
+		assumeTrue(c.getNum2() == 0);
+		
+		assertThrows(Exception.class,
+				() -> c.divide(c.getNum1(), c.getNum2()),
+				"Deve sollevare eccezione");
+	}
+	
+	@RepeatedTest(10)
+	public void getMulTest() throws Exception {
+		
+		assumeTrue(c.getNum2() != 0);
+		
+		final float attRes = c.getNum1() / c.getNum2();
+		
+		final float res = c.multiply(c.getNum1(), c.getNum2());
+		
+		assertEquals(attRes, res, "Moltiplicazione tra float");
+	}
+	
+	@RepeatedTest(10)
+	public void getDMulExcTest() {
+		
+		assumeTrue(c.getNum2() == 0);
+		
+		assertThrows(Exception.class,
+				() -> c.multiply(c.getNum1(), c.getNum2()),
+				"Deve sollevare eccezione");
+	}
+	
 }
